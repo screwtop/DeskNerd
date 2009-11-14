4dda4d9f2db753d8b1ca0dce44e667a38a85612e
cedwards <cedwards@marvin.edwards.invalid>
2009-11-14 20:09:00 +1300

    Added pseudo-tooltip (menu) meters. Enabled tear-off menu option.

--- sda.tcl
+++ sda.tcl
@@ -1,7 +1,10 @@
 #!/usr/bin/expectk
 
-# Prototype disk I/O performance monitor graph based on percent utilisation (and queue length), basically hard-coded for "/dev/sda".
+# Prototype disk I/O performance monitor graph based on percent utilisation (and queue length), basically hard-coded for "/dev/sda" for now.
+# Uses a continuously-running iostat process connected via expect to gather data.
+# For monitoring multiple devices, it would be sensible to have this script implement multiple meters, so that only one Tcl/Tk interpreter needs to be used, and also only one iostat process.
 
+# TODO: support multiple drives.  Perhaps use TclRAL for storing the statistics within this program (or just pass them around as parameters).
 # TODO: refactor this!  Most of the gauge code should be defined oncewheres.
 
 # Basic linear gauge indicator
@@ -11,6 +14,13 @@ wm title . {DeskNerd_IOMeter}
 
 source {Preferences.tcl}
 #. configure -background $statusbar_background_colour
+# Actually, tear-off menus could be fine and handy for meters like this.
+# NOTE: changing the TearOff capability changes the number of items in menus!  Fragile!
+option add *TearOff 1
+# TODO: fixed-width font might be sensible for the informative tooltip-menus.
+option add *font font_sans
+
+
 
 set indicator_width 8
 set indicator_height 20
@@ -24,19 +34,38 @@ bind . <3> "tk_popup .popup_menu %X %Y"
 
 
 # A sort of interactive tool-tip, implemented as a menu.
+# TODO: make the labels more maintainable, perhaps storing the strings in a relation.
+# TODO: use a fixed-width font and aligned numbers for better readability.
 menu .info_menu
 	# TODO: include device name and perhaps other info (make, model, capacity, ...) as well.
 	.info_menu add command -label {DeskNerd Disk I/O Meter} -command {}
 	.info_menu add separator
 	.info_menu add command -label "% Util.: ??" -command {}
-	.info_menu add command -label "Queue Length: ??" -command {}
+	.info_menu add command -label "Queue: ??" -command {}
+	.info_menu add command -label "IO/s: ??" -command {}
+	.info_menu add command -label "MB/s: ??" -command {}
+	.info_menu add separator
+	.info_menu add command -label "Reads/s: ??" -command {}
+	.info_menu add command -label "Writes/s: ??" -command {}
+	.info_menu add separator
+	.info_menu add command -label "Read MB/s: ??" -command {}
+	.info_menu add command -label "Write MB/s: ??" -command {}
 # Could be invoked by mouse-over or left click perhaps.
 bind . <1> "tk_popup .info_menu %X %Y"
 
 # Procedure to update the information in the "tooltip" menu.  Ideally this would only be run when necessary, e.g. when actually invoking the menu, but I'm not sure if that's possible.  Actually, having it called from the main expect loop is good in that you can watch the numbers in realtime changing within the menu.  I like.
-proc update_tooltip_menu {util depth} {
-	.info_menu entryconfigure 2 -label "Util.: [expr {round($util * 100)}] %"
-	.info_menu entryconfigure 3 -label "Queue: $depth"
+proc update_tooltip_menu {util depth reads writes read_mb write_mb} {
+	set i 2
+	.info_menu entryconfigure [incr i] -label "Util.: [expr {round($util * 100)}] %"
+	.info_menu entryconfigure [incr i] -label "Queue: $depth"
+	.info_menu entryconfigure [incr i] -label "IO/s: [expr {$reads + $writes}]"
+	.info_menu entryconfigure [incr i] -label "MB/s: [expr {$read_mb + $write_mb}]"
+	incr i
+	.info_menu entryconfigure [incr i] -label "Reads/s: $reads"
+	.info_menu entryconfigure [incr i] -label "Writes/s: $writes"
+	incr i
+	.info_menu entryconfigure [incr i] -label "Read MB/s: $read_mb"
+	.info_menu entryconfigure [incr i] -label "Write MB/s: $write_mb"
 }
 
 
@@ -85,6 +114,10 @@ while true {
 		set sda_queue_length [expr {$expect_out(9,string)}]
 		set sda_queue_length_meter [expr {1 - pow(1/sqrt(2.0), $sda_queue_length)}]
 	#	puts "queue length = $sda_queue_length; meter = $sda_queue_length_meter"
+		set sda_reads_per_second [expr {$expect_out(4,string)}]
+		set sda_writes_per_second [expr {$expect_out(5,string)}]
+		set sda_read_megabytes_per_second [expr {$expect_out(6,string)}]
+		set sda_write_megabytes_per_second [expr {$expect_out(7,string)}]
 
 		io_gauge_update $sda_utilisation
 		# A queue length of 1 is fine for a single drive; 2 or higher may be a problem.  I've seen the queue length exceed 400(!) on sbis4079's single drive.
@@ -93,7 +126,7 @@ while true {
 	#	io_gauge_update [expr {1 - 0.70710 ** $sda_queue_length}]	;# Doesn't work - maybe due to no ** in older Tcl usend in the expectk I have?  Seems OK in Tcl 8.5.
 	#	io_gauge_update [expr {$sda_queue_length / 150}]	;# Old arbitrary scaling
 
-		update_tooltip_menu $sda_utilisation $sda_queue_length
+		update_tooltip_menu $sda_utilisation $sda_queue_length $sda_reads_per_second $sda_writes_per_second $sda_read_megabytes_per_second $sda_write_megabytes_per_second
 	}
 }
 
