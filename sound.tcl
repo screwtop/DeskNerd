21fd981a0bc587b929a4e9374500c5dc9c9bcf0a
cedwards <cedwards@marvin.edwards.invalid>
2009-11-15 19:28:30 +1300

    Adjusted metering for more effective range while retaining sensible dB-based colouring, by moving the dB FS to meter scale conversion into the gauge update procedure.

--- sound.tcl
+++ sound.tcl
@@ -2,6 +2,7 @@
 
 # DeskNerd component for displaying audio output level.  I use JACK pretty much all the time, so this is just implemented by running Nicholas J Humfrey's console-based jackmeter program in the background.  Not sure if we can connect it to an output port though, huh?  Oh, well.
 # TODO: probably want two meters for stereo monitoring, but maybe one is fine (jackmeter will mix all its inputs down anyway).
+# TODO: add menu for ease of exiting.
 
 
 wm title . {DeskNerd_SoundMeter}
@@ -10,7 +11,7 @@ set application_name {DeskNerd Sound Meter}
 source {Preferences.tcl}
 source {every.tcl}
 
-set refresh_frequency 30	;# Will just be passed to jack_meter, which will then drive this program's output.
+set refresh_frequency 10	;# Will just be passed to jack_meter, which will then drive this program's output.
 
 # Basic meter dimension preferences:
 set indicator_width 8
@@ -25,16 +26,19 @@ place [frame .sound_gauge.meter     -width [expr {$indicator_width-2}] -height 0
 
 
 # Update the gauge:
-proc sound_gauge_update {value} {
+proc sound_gauge_update {decibels} {
 	global indicator_height .sound_gauge.meter
 
 	# Last 6 dB may get grungy, but 20 dB is commonly considered reasonable headroom, and last 3 dB is "precious", so we'll make that red.
-	# Mind you, jackmeter uses peak-reading metering.  And 14 dB is probably more likely for popular music.
-	if     {$value >= 0.99} then {set gauge_colour red} \
-	elseif {$value >= 0.97} then {set gauge_colour orange} \
-	elseif {$value >= 0.94} then {set gauge_colour yellow} \
-	else                         {set gauge_colour green}
+	# Mind you, jackmeter uses peak-reading metering.  And 14 dB is probably more likely for popular music.  Replay Gain tends to give you -3 dB K-14, with "nominal peaks" around 9 dB K-14, or -6 dB FS (peak).  So, we'll put the orange/green transition at -6 dB here.
+	if     {$decibels >= -1} then {set gauge_colour red} \
+	elseif {$decibels >= -3} then {set gauge_colour orange} \
+	elseif {$decibels >= -6} then {set gauge_colour yellow} \
+	else                          {set gauge_colour green}
 
+	# Map dB peak re. full scale to indicator height.  Perhaps convert to Stevens loudness (better if we had RMS levels to work with).
+#	set value [expr {$decibels / 100.0 + 1}]	;# 100 dB range
+	set value [expr {$decibels / 20 + 1}]
 	.sound_gauge.meter configure -height [expr {$value * ($indicator_height-2)}] -background $gauge_colour
 }
 
@@ -50,13 +54,14 @@ while true {
 	}
 
 #	puts $sound_level_peak
-	set sound_level_peak_normalised [expr {$sound_level_peak / 100.0 + 1.0}]
-	puts $sound_level_peak_normalised
+#	set sound_level_peak_normalised [expr {$sound_level_peak / 100.0 + 1.0}]
+#	puts $sound_level_peak_normalised
+
 	# Update info menu-panel:
 #	update_tooltip_menu $ram_total $ram_used $effective_ram_used $ram_free $effective_ram_free $mem_shared $mem_buffer $mem_cached $swap_total $swap_used $swap_free
 
-	
-	sound_gauge_update $sound_level_peak_normalised
+	sound_gauge_update $sound_level_peak
+#	sound_gauge_update $sound_level_peak_normalised
 #	sound_gauge_update [expr {$sound_level_peak / 100.0 + 1}]
 }
 
