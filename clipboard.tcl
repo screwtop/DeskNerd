07eacaf37a64e0bef91cbeb9725d8553740114c6
Chris Edwards <chris.edwards@otago.ac.nz>
2015-01-20 15:42:56 +1300

    Factored out debugging output routines.

--- clipboard.tcl
+++ clipboard.tcl
@@ -29,20 +29,7 @@ package require Tk
 wm title . {DeskNerd_Clipboard}
 
 set ::debugging false
-
-# Various alert routines for different reasons/severity.  Could maybe use different beep patterns.  Don't forget that there's already a Tcl "error" command to avoid.
-proc alert {message} {
-        puts stderr "\x1b\[31;1m >> ALERT: ${message}\x1b\[0m"; flush stderr
-}
-
-proc warning {message} {
-        puts stderr "\x1b\[33;1m >> WARNING: ${message}\x1b\[0m"; flush stderr
-}
-
-proc debug {message} {
-        if {$::debugging} {puts stderr "\x1b\[35;1m >> DEBUG: $message\x1b\[0m"; flush stderr}
-}
-
+source debugging.tcl
 
 # Tolerate absence of tzint:
 if {[catch {package require tzint}]} {
