0da3154d7c4452a804e80fbbaeb5b8b5a8ccb2d1
Chris Edwards <chris.edwards@otago.ac.nz>
2016-06-14 13:27:12 +1200

    Switching to /proc/meminfo as source for memory meter

--- memory.tcl
+++ memory.tcl
@@ -1,15 +1,20 @@
+#!/usr/bin/env wish
 #!/usr/bin/wish8.5
+# or perhaps /usr/bin/env wish
 
 # Basic memory usage indicator for DeskNerd.  We could show cache, swap, wired, etc. but what we really care about is how much physical RAM is used out of the available physical RAM.
-# Works by spawning Linux's "free" command in repeat-report mode (-s <nsecs>) and parsing its output.
+# Now (2015-07-13) works by reading from /proc/meminfo (`free`'s output varies depending on the version n stuff).
+# From Linux 3.2, MemAvailable is probably quite a useful metric.
+# {slurp /proc/meminfo} only takes about 10 microseconds, so no biggie there.
+# Originally worked by spawning Linux's "free" command in repeat-report mode (-s <nsecs>) and parsing its output.
 
 # I was having problems with the original expect-based version consuming huge amounts of memory over time.  Don't know if it was pipe buffers never being cleared or what, but this is another try using plain Tcl pipe I/O. 2012-08-23.
 
 # TODO: add a (separate, shareable) proc for converting bytes to MiB, GiB, etc., perhaps automatically according to the magnitude.  Or embed a Frink interpreter via TclBlend?! ;^)
 
 
-#set application_name {DeskNerd Memory Meter}
-set application_name {DeskNerd_Memory_Meter}
+set application_name {DeskNerd Memory Meter}
+#set application_name {DeskNerd_Memory_Meter}
 wm title . $application_name
 
 source {Preferences.tcl}
@@ -24,12 +29,41 @@ set refresh_interval_s 2	;# Memory utilisation doesn't normally change very rapi
 option add *TearOff 1
 # TODO: fixed-width font might be sensible for the informative tooltip-menus.
 #option add *font font_mono
-
+#option add *font {{Letter Gothic 12 Pitch} 10}
 #set ::env(TERM) dumb	;# to avoid ANSI codes from dstat
 
 # Basic meter dimension preferences:
 set indicator_width 8
 set indicator_height 20
+
+set use_proc_meminfo 1
+set update_interval_milliseconds 1000
+
+
+# Read a file's contents:
+proc slurp {filename} {
+	set file_handle [open $filename r]
+	set file_data [read $file_handle]
+	close $file_handle
+	return $file_data
+}
+
+
+# Read current memory stats from /proc/meminfo:
+proc read_meminfo {} {
+	array unset ::memory_stat
+
+	set meminfo [slurp /proc/meminfo]
+
+	# Convert into array for ease of reference:
+	foreach statistic [split $meminfo "\n"] {
+	#	puts "<<$statistic>>"
+		regexp {^(.*): +([0-9]+) ?(.*)$} $statistic entire_match statistic_name statistic_value statistic_unit
+		set ::memory_stat($statistic_name) $statistic_value
+		# TODO: keep $statistic_unit as well?  They're all kiB at the moment, but in future...?
+	}
+}
+
 
 # Pop-up menu for convenient exiting:
 menu .popup_menu
@@ -108,6 +142,37 @@ proc memory_gauge_update {value} {
 }
 
 
+if {$use_proc_meminfo} {
+	every $update_interval_milliseconds {
+
+		read_meminfo
+
+		# Boringly copy stuff from the resulting array into global variables, so I don't have to rewrite update_tooltip_menu!  TODO: probably nicer to rewrite the update_ procs and the old `free`-based code to use the array variables.  However, some of these require a little calculation, so are still required.
+		set ::ram_total  $::memory_stat(MemTotal)
+		set ::swap_total $::memory_stat(SwapTotal)
+		set ::swap_free  $::memory_stat(SwapFree)
+		set ::swap_used  [expr {$::memory_stat(SwapTotal) - $::memory_stat(SwapFree)}]
+		set ::mem_shared     $::memory_stat(Shmem)
+		set ::mem_buffer     $::memory_stat(Buffers)
+		set ::mem_cache      $::memory_stat(Cached)
+		# TODO: Slab?
+		set ::effective_ram_free $::memory_stat(MemAvailable) ;# If MemAvailable statistic exists!  Otherwise calculate.
+		set ::effective_ram_used [expr {$::memory_stat(MemTotal) - $::memory_stat(MemAvailable)}]
+
+
+
+		# Update info menu-panel:
+		if {[info exists ::ram_total] && [info exists ::swap_total]} {
+			update_tooltip_menu
+	#	update_tooltip_menu $::ram_total $::ram_used $::effective_ram_used $::ram_free $::effective_ram_free $::mem_shared $::mem_buffer $::mem_cached $::swap_total $::swap_used $::swap_free
+	
+		# For display, the effective physical RAM utilisation is the important thing (filesystem cache will make room for processes if necessary).
+		# NOTE: from Linux 3.2, there's also a MemAvailable in /proc/meminfo, which is probably the best thing to use.
+			memory_gauge_update [expr {double($::effective_ram_used) / double($::ram_total)}]
+		}
+	}
+} else {
+
 
 #log_user 0
 #set timeout [expr {$refresh_interval_s + 1}]	;# or perhaps * 2.
@@ -122,6 +187,14 @@ fconfigure $input_stream -buffering line
 #-/+ buffers/cache:    2072904    1860832
 #Swap:      2048276      74432    1973844
 #>>
+
+# However, the output may vary with the version.  For "free from procps-ng 3.3.10", it's:
+#<<
+#              total        used        free      shared  buff/cache   available
+#Mem:        8087480     1041668     1595552       91572     5450260     6670524
+#Swap:       8388604           0     8388604
+#>>
+# TBH, maybe just reading from /proc/meminfo would be just as easy, and possibly less likely to need altering over time.
 
 # Hmm, ideally an atomic read from the input stream from `free` would guarantee one "Mem" and one "Swap" line.
 
@@ -154,6 +227,7 @@ proc read_input {input_stream} {
 	}
 	# TODO: handle eof properly.
 #	expect eof {break}	;# i.e. not like this, which just causes an expect timeout, since it waits for EOF at every iteration of this while loop.
+}
 }
 
 puts "Quitting."
